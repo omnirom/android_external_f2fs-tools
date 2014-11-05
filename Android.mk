a1ee6b22f0c1b8d7bfeca5a24d7b9da4e1694392
XpLoDWilD <me@xplod.fr>
2014-11-05 16:47:54 +0000

    Android.mk: Remove duplicate declarations
    Change-Id: I975469b5df3d480a57ccfd2c3cbaec9756fab686

--- Android.mk
+++ Android.mk
@@ -7,26 +7,10 @@ LOCAL_MODULE_TAGS := optional
 include $(BUILD_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
-LOCAL_SRC_FILES := mkfs/main.c
-LOCAL_MODULE := mkfs.f2fs
-LOCAL_MODULE_TAGS := optional
-LOCAL_FORCE_STATIC_EXECUTABLE := true
-LOCAL_STATIC_LIBRARIES := libmake_f2fs libcutils liblog libc
-include $(BUILD_EXECUTABLE)
-
-include $(CLEAR_VARS)
 LOCAL_SRC_FILES := fsck/main.c fsck/fsck.c fsck/dump.c fsck/mount.c lib/libf2fs.c
 LOCAL_MODULE := libfsck_f2fs
 LOCAL_MODULE_TAGS := optional
 include $(BUILD_STATIC_LIBRARY)
-
-include $(CLEAR_VARS)
-LOCAL_SRC_FILES := fsck/fsck_f2fs_main.c
-LOCAL_MODULE := fsck.f2fs
-LOCAL_MODULE_TAGS := optional
-LOCAL_FORCE_STATIC_EXECUTABLE := true
-LOCAL_STATIC_LIBRARIES := libfsck_f2fs libcutils liblog libc
-include $(BUILD_EXECUTABLE)
 
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := fsck/fibmap.c
